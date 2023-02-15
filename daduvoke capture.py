20c175dbab276b87a27e3a915c03a4ac84fb03d2
daduvoke <88345701+DaduVoke@users.noreply.github.com>
2023-02-15 06:09:19 +0400

    Update daduvoke capture.py

--- daduvoke capture.py
+++ daduvoke capture.py
@@ -9,30 +9,28 @@ class MainWindow:
 
         self.tcpdump_process = None
 
-        # Create the capture button
+        
         self.capture_button = tk.Button(master, text="DV c4ptur1ng", command=self.start_capture)
         self.capture_button.pack(pady=10)
 
-        # Create the stop button
         self.stop_button = tk.Button(master, text="St0p", command=self.stop_capture, state="disabled")
         self.stop_button.pack(pady=10)
 
-        # Create the monitoring area
         self.monitoring_area = tk.Text(master, height=20, width=80)
         self.monitoring_area.pack()
 
     def start_capture(self):
-        # Start the tcpdump process in a separate thread
+        
         self.tcpdump_process = subprocess.Popen(["tcpdump", "-i", "eth0"], stdout=subprocess.PIPE)
         self.stop_button.config(state="normal")
         self.capture_button.config(state="disabled")
 
-        # Start a separate thread to read the tcpdump output
+        
         self.tcpdump_thread = threading.Thread(target=self.read_tcpdump_output)
         self.tcpdump_thread.start()
 
     def stop_capture(self):
-        # Terminate the tcpdump process
+        
         self.tcpdump_process.terminate()
         self.tcpdump_process = None
         self.stop_button.config(state="disabled")
@@ -44,14 +42,13 @@ class MainWindow:
             if not line:
                 break
 
-            # Update the monitoring area with the tcpdump output
+            
             self.monitoring_area.insert(tk.END, line.decode())
             self.monitoring_area.see(tk.END)
 
-            # Add your code to analyze the packet here
-            # ...
+            
 
 root = tk.Tk()
 app = MainWindow(root)
-app.start_capture() # Start capturing packets when the application starts
+app.start_capture() 
 root.mainloop()
